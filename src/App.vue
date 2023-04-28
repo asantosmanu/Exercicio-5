c14aa2a472b1331e2b25be30d511558c7e9be619
asantosmanu <manuelasantos452006@gmail.com>
2023-04-28 15:40:11 -0300

    exercício-1e2

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, computed } from 'vue'
+const cidades = ['São Paulo','Rio de Janeiro','Belo Horizonte','Salvador','Fortaleza','Curitiba','Manaus','Recife','Porto Alegre','Brasília']
+
+</script>
+
+<template>
+<ul>
+  <li v-for="(cidade, index) in cidades.sort()" :key="index">{{ cidade }}</li>
+</ul>
+</template>
+
+<style scoped>
+</style>
